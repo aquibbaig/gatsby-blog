234249009c3693a8abb50a03e99c2d6364eef407
Aquib Baig <aquibbaig97@gmail.com>
2019-04-28 16:12:55 +0530

    Revert "add disqus to each blogpost"

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,18 +6,12 @@ import Bio from '../components/Bio'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import { rhythm, scale } from '../utils/typography'
-import { DiscussionEmbed } from "disqus-react"
 
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.mdx
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
-    const disqusShortname = "aquibbaig";
-    const disqusConfig = {
-      identifier: post.id,
-      title: post.frontmatter.title,
-    };
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -65,7 +59,6 @@ class BlogPostTemplate extends React.Component {
             )}
           </li>
         </ul>
-        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
       </Layout>
     )
   }
